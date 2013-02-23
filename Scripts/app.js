fce0a113ac38951cdb3006cd8aa5ad30e9cff374
koty <kouichi.nishizawa+github@gmail.com>
2013-02-23 11:07:15 +0900

    マーカーの設定、UTF8化

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,13 +1,25 @@
-var app = (function () {
+﻿var app = (function () {
     function app() { }
     app.initialize = function initialize() {
-        var latlng = new google.maps.LatLng(35.709984, 139.810703);
+        var latlng = new google.maps.LatLng(35.630442, 139.882951);
         var opts = {
             zoom: 15,
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+        var m_latlng1 = new google.maps.LatLng(35.632605, 139.88132);
+        var marker1 = new google.maps.Marker({
+            position: m_latlng1,
+            map: map,
+            title: "デズニーランド（行ったことない）"
+        });
+        var m_latlng2 = new google.maps.LatLng(35.625663, 139.884238);
+        var marker2 = new google.maps.Marker({
+            position: m_latlng2,
+            map: map,
+            title: "デズニーシー（行ったことない）"
+        });
     };
     return app;
 })();
--- Scripts/app.ts
+++ Scripts/app.ts
@@ -1,13 +1,28 @@
-/// <reference path="../typings/google.maps.d.ts" />
+﻿/// <reference path="../typings/google.maps.d.ts" />
 
 class app {
     public static initialize(): void {
-        var latlng = new google.maps.LatLng(35.709984, 139.810703);
+        var latlng = new google.maps.LatLng(35.630442, 139.882951);
         var opts = {
             zoom: 15,
             center: latlng,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+        var map = new google.maps.Map(
+                document.getElementById("map_canvas"), opts);
+
+        var m_latlng1:google.maps.LatLng = new google.maps.LatLng(35.632605, 139.88132);
+        var marker1: google.maps.Marker = new google.maps.Marker({
+            position: m_latlng1,
+            map: map,
+            title: "デズニーランド（行ったことない）"
+        });
+
+        var m_latlng2: google.maps.LatLng = new google.maps.LatLng(35.625663, 139.884238);
+        var marker2: google.maps.Marker = new google.maps.Marker({
+            position: m_latlng2,
+            map: map,
+            title: "デズニーシー（行ったことない）"
+        });
     }
 }
